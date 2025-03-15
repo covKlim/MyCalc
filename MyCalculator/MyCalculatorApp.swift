f6533ceee78dd3f432316142dc5cddd2df1a5ee7
Klim_Kovalevsky <covKlim@yandex.ru>
2025-03-15 13:17:01 +0300

    Initial Commit

---
+++ MyCalculator/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  MyCalculator
+//
+//  Created by Клим Ковалевский on 15.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCalculatorTests/MyCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorTests.swift
+//  MyCalculatorTests
+//
+//  Created by Клим Ковалевский on 15.03.2025.
+//
+
+import Testing
+@testable import MyCalculator
+
+struct MyCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
